127efd70fc18658655be93dfac94ed2e22ba5db5
Shaswat Sunder Nayak <shaswatsunder@gmail.com>
2014-06-03 01:42:04 +0530

    Update models.py

--- forum/models.py
+++ forum/models.py
@@ -5,100 +5,137 @@ from django.db.models.signals import post_save
 from taggit.managers import TaggableManager
 from shared.utils import *
 
-class UserProfile(Model):
-    avatar = ImageField("Profile Pic", upload_to="avatar", blank=True, null=True)
-    posts = IntegerField(default=0)
-    replies = IntegerField(default=0)
-    user = OneToOneField(User, related_name="profile")
 
-    def __unicode__(self):
-        return self.user.username
-
-    def increment_posts(self):
-        self.posts += 1
-        self.save()
-
-    def increment_replies(self):
-        self.replies += 1
-        self.save()
-
-class Category(Model):
-    title = CharField(max_length=60)
-    description = TextField(max_length=500)
-
-    def __unicode__(self):
-        return self.title
-
-    def get_absolute_url(self):
-        return reverse2("forum", dpk=self.pk)
-
-
-class Post(Model):
-    title = CharField(max_length=60)
-    created = DateTimeField(auto_now_add=True)
-    creator = ForeignKey(User, blank=True, null=True)
-    body = TextField()
-    category = ForeignKey(Category, related_name="posts")
-    tags = TaggableManager()
-    vote = IntegerField(default=0)
-    
-    
-    class Meta:
-        ordering = ["created"]
-
-    def __unicode__(self):
-        return u"%s" % (self.title)
-
-    def increment_vote(self):
-        self.vote += 1
-        self.save()                
-
-    def short(self):
-        created = self.created.strftime("%b %d, %I:%M %p")
-        return u"%s - %s\n%s" % (self.creator, self.title, created)
-
-    def profile_data(self):
-        p = self.created.profile
-        return p.posts, p.avatar
+class Post(models.Model):
+        title = CharField(max_length=60)
+	body=models.TextField()
+	created_date=models.DateTimeField(auto_now_add=True)
+	#fk
+	user=models.ForeignKey('UserProfile')
+	#fk
+	category=models.ForeignKey('Category')
+	count=models.IntegerField(default=0)
+	admin_id=models.IntegerField()#question will need admin's approval
+	
+	class Meta:
+        	ordering = ["created_date"]        
+	
+	def __unicode__(self):
+	      return self.title
+	def increment_count(self):
+        	self.count += 1
+        	self.save() 
+	def short(self):
+        	created = self.created_date.strftime("%b %d, %I:%M %p")
+        	return u"%s - %s\n%s" % (self.user, self.title, created_date)
+	def profile_data(self):
+        	p = self.created_date.profile
+        	return p.posts, p.avatar
 
 
-class Reply(Model):
-    title = ForeignKey(Post, related_name="post")
-    created = DateTimeField(auto_now_add=True)
-    creator = ForeignKey(User, blank=True, null=True)
-    body = TextField()
-    vote = IntegerField(default=0)
+class Reply(models.Model):
+	#fk	
+	title=models.ForeignKey('Post')
+	body=models.TextField()
+	user=models.ForeignKey('User')
+	post_date=models.DateTimeField(auto_now_add=True)
+	file_upload=models.FileField(upload_to='forum/file')
+	ratings=models.IntegerField(max_length=5,default=0)# there should be a limit within which the rating should be done
+	admin_approved=models.BooleanField(default=False)
+	count=models.IntegerField(default=0)
+
+	class Meta:
+        	ordering = ["post_date"]
+ 	
+	def __unicode__(self):
+  	      return self.title
+	def increment_count(self):
+        	self.count += 1
+        	self.save()
+	def short(self):
+        	created = self.post_date.strftime("%b %d, %I:%M %p")
+        	return u"%s - %s\n%s" % (self.user, self.title, post_date)
+	def profile_data(self):
+        	p = self.post_date.profile
+        	return p.posts, p.avatar
 
 
-    class Meta:
-        ordering = ["created"]
-
-    def __unicode__(self):
-        return u"%s" % (self.title)
-
-    def increment_vote(self):
-        self.vote += 1
-        self.save()                
-
-    def short(self):
-        created = self.created.strftime("%b %d, %I:%M %p")
-        return u"%s - %s\n%s" % (self.creator, self.title, created)
-
-    def profile_data(self):
-        p = self.created.profile
-        return p.posts, p.avatar
 
 
-class Comment(Model):
-    comment = ForeignKey(Reply)
-    body = TextField()
-    created = DateTimeField(auto_now_add=True)
-    creator = ForeignKey(User, blank=True, null=True)
-    vote = IntegerField(default=0)
+class Comment(models.Model):
+	#fk
+	answer=models.ForeignKey('Answer')
+	text=models.TextField()
+	created_date=models.DateTimeField(auto_now_add=True)
+	count=models.IntegerField(default=0)
+	user=models.ForeignKey('User')
+	
+	class Meta:
+        	ordering = ["-created"]
+	
+	def __unicode__(self):
+  	      return self.count
+	
+class UserProfile(models.Model):
+	tab_id=models.IntegerField(primary_key=True)
+	#username=models.CharField(max_length=20)
+	#email=models.EmailField(max_length=30)
+        user = OneToOneField(User)
+	location=models.CharField(max_length=10)
+	nickname=models.CharField(max_length=10)# May not needed
+	#password = models.CharField(max_length=15)
+	avatar= models.ImageField(upload_to='static/images/profile_image', blank=True)
+	online_status=models.BooleanField(default=False)# Good!
+	user_type=models.IntegerField(max_length=1,default=0)
+	skills=models.CharField(max_length=100, blank=True)#to mantain user profile
+	posts = IntegerField(default=0)
+	reply_count=models.IntegerField(default=0)
+	role=models.IntegerField()#to differentiate b/w user and admin
 
-    class Meta:
-        ordering = ["-created"]
+	def __unicode__(self):
+  	      return self.username
 
-    def __unicode__(self):
-        return unicode("%s - %s(title)" % (self.title, self.forum))
+	def increment_posts(self):
+        	self.posts += 1
+        	self.save()
+
+	def increment_replies(self):
+        	self.reply_count += 1
+        	self.save()
+	
+class Category(models.Model):
+	category=models.CharField(max_length=20)
+	tags = models.TaggableManager()
+	
+	def __unicode__(self):
+  	      return self.category
+
+class Ticket(models.Model):
+	user_id=models.ForeignKey('User')
+	topic_id=models.ForeignKey('Topics')
+	message=models.CharField()
+	ticket_id=models.IntegerField()
+	file_uploads=models.FileField(upload_to='tickets/file')
+	created_date_time=models.DateTimeField(auto_now_add=True)
+	overdue_date_time=models.DateTimeField(auto_now_add=True)
+	closed_date_time=models.DateTimeField(auto_now_add=True)
+	status=models.IntegerField()
+	reopened_date_time=models.DateTimeField(auto_now_add=True)
+	topic_priority=models.IntegerField()
+	duration_for_reply=models.IntegerField()
+	
+	def __unicode__(self):
+	      return self.user_id
+
+
+class Tablet_info(models.Model):
+	rcID=models.IntegerField()
+	rcName=models.CharField()
+	start_tab_id=models.IntegerField()
+	end_tab_id=models.IntegerField()
+	count=models.IntegerField()
+	location=models.CharField()
+
+	def __unicode__(self):
+	      return self.start_tab_id,self.end_tab_id
 
